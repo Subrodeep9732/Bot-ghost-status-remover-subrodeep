4e427b12e4f2757f37c850f9ee62fa1b75152367
Subrodeep <125560440+Subrodeep9732@users.noreply.github.com>
2025-03-14 22:12:40 +0530

    index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["Listening to Royal S Gaming"];
+const statusMessages = ["🎧 Listening to Toy", "🎮 Watching Wink's Teapot"];
 
 
 let currentIndex = 0;
